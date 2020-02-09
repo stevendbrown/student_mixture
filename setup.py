e14e89eff54c8569f5c8fa30a2d264a4fb74269a
omritomer <omritomer1@mail.tau.ac.il>
2020-02-09 12:22:13 +0200

    some fixes

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/omritomer/student_mixture',
-    version='0.1.10',
+    version='0.1.11',
     zip_safe=False,
 )
