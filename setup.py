1dc01b09ee58649cc70200d2861c9976ea3e557c
omritomer <omritomer1@mail.tau.ac.il>
2020-02-09 12:18:47 +0200

    some fixes

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [str(r.req) for r in parse_requirements('requirements_dev.txt')]
+requirements = [str(r.req) for r in parse_requirements('requirements_dev.txt', session=False)]
 
 setup_requirements = ['pytest-runner', ]
 
