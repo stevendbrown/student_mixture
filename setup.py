804087d8389105dd64cc250ae718769479369ecc
omritomer <omritomer1@mail.tau.ac.il>
2020-02-09 12:15:49 +0200

    some fixes

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@
 """The setup script."""
 
 from setuptools import setup, find_packages, Extension
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
@@ -11,7 +17,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [str(r.req) for r in parse_requirements('requirements_dev.txt')]
 
 setup_requirements = ['pytest-runner', ]
 
@@ -47,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/omritomer/student_mixture',
-    version='0.1.8',
+    version='0.1.10',
     zip_safe=False,
 )
